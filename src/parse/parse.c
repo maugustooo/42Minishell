df803ff62817806a3014e5c21f2eab2dbd6cd6fe
Marco Augusto Goncalves Amorim <maugusto@c2r1s1.42porto.com>
2024-07-29 14:08:35 +0100

    Minishell in prompt

--- src/parse/parse.c
+++ src/parse/parse.c
@@ -6,7 +6,7 @@
 /*   By: maugusto <maugusto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/29 12:35:32 by maugusto          #+#    #+#             */
-/*   Updated: 2024/07/29 13:09:17 by maugusto         ###   ########.fr       */
+/*   Updated: 2024/07/29 14:02:24 by maugusto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@ void *get_line(t_mini *mini)
 {
 	char *line;
 	void(*mini);
-	line = readline("What i'v done: ");
+	line = readline("MiniShell: ");
 	ft_printf("%s", line);
 }
