41d7feb8ce5b97e5c09df4b09db1b2be893e273e
Marco Augusto Goncalves Amorim <maugusto@c2r1s1.42porto.com>
2024-07-29 15:51:35 +0100

    I add the parse and tokens handle i also added the list and the struct in header file

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: maugusto <maugusto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/29 11:20:52 by maugusto          #+#    #+#             */
-/*   Updated: 2024/07/29 13:09:11 by maugusto         ###   ########.fr       */
+/*   Updated: 2024/07/29 14:33:54 by maugusto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 
 int main(int argc, char **argv)
 {
-	t_mini *mini;
+	(void)argc;
+	(void)argv;
+	t_mini mini;
 	
 	get_line(&mini);
 }
--- src/parse/parse.c
+++ src/parse/parse.c
@@ -6,16 +6,22 @@
 /*   By: maugusto <maugusto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/29 12:35:32 by maugusto          #+#    #+#             */
-/*   Updated: 2024/07/29 14:02:24 by maugusto         ###   ########.fr       */
+/*   Updated: 2024/07/29 15:48:15 by maugusto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
+/**
+ * @brief Get the line in prompt
+ * 
+ * @param mini The struct of the minishell
+ * @return void* 
+ */
 void *get_line(t_mini *mini)
 {
-	char *line;
-	void(*mini);
-	line = readline("MiniShell: ");
-	ft_printf("%s", line);
+	char *rl;
+	rl = readline("MiniShell: ");
+	mini->line = rl;
+	ft_printf("%s", mini->line);
+	get_tokens(mini);
 }
