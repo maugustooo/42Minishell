78c308500908c9e4cfd64656c4d37615de775b1c
Marco Augusto Goncalves Amorim <maugusto@c2r1s1.42porto.com>
2024-07-29 10:49:47 +0100

    pasta headers com header e libft

---
+++ Includes/minishell.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maugusto <maugusto@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/29 10:23:59 by maugusto          #+#    #+#             */
+/*   Updated: 2024/07/29 10:28:56 by maugusto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+
+# include <curses.h>
+# include <term.h>
+# include <termios.h>
+# include <sys/ioctl.h>
+# include <dirent.h>
+# include <sys/stat.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include "libft/libft.h"
+
+#endif
