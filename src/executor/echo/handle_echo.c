b5054c5aec8fda325eee32e5ec4fb93586ad40f2
Dot99 <gui.rocha.felix@gmail.com>
2024-07-31 08:17:43 +0100

    Change in echo

--- src/executor/echo/handle_echo.c
+++ src/executor/echo/handle_echo.c
@@ -6,19 +6,20 @@
 /*   By: gude-jes <gude-jes@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/30 15:33:54 by gude-jes          #+#    #+#             */
-/*   Updated: 2024/07/30 15:37:19 by gude-jes         ###   ########.fr       */
+/*   Updated: 2024/07/31 08:17:08 by gude-jes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 //Sus
-void    handle_echo(t_token *flag, t_token *text)
+void	handle_echo(t_token *flag, t_token *text)
 {
-    if(!flag && !text)
-        ft_printf("\n");
-    else if (!text)
-        //TODO: Enter Readline again
-    else if(!flag)
-        ft_printf("%s\n", text);
+	int	nline;
+
+	if (flag && ft_strncmp(flag, "-n", ft_strlen(flag)) == 0)
+		nline = 1;
+	ft_printf("%s", text);
+	if (nline)
+		ft_printf("\n");
 }
