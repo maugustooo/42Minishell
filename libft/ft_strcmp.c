b294aa25bb2ca9aab4db8b5791a19c7c7797eec5
Dot99 <gui.rocha.felix@gmail.com>
2024-08-20 17:00:40 +0100

    Comments ft_strcmp + prog expander

--- libft/ft_strcmp.c
+++ libft/ft_strcmp.c
@@ -6,12 +6,26 @@
 /*   By: gude-jes <gude-jes@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/01 10:48:46 by gude-jes          #+#    #+#             */
-/*   Updated: 2024/08/01 10:56:11 by gude-jes         ###   ########.fr       */
+/*   Updated: 2024/08/20 16:41:54 by gude-jes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/**
+ * @defgroup mandatory Mandatory
+ * @{
+ * @file ft_strcmp.c
+ * @brief Function to compare strings
+*/
+
 #include "libft.h"
 
+/**
+ * @brief Compares two strings
+ * 
+ * @param s1 First String
+ * @param s2 Second String
+ * @return int Difference between s1 and s2
+*/
 int	ft_strcmp(const char *s1, const char *s2)
 {
 	int	i;
@@ -25,3 +39,5 @@ int	ft_strcmp(const char *s1, const char *s2)
 	}
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
+
+/**@}*/
--- src/expander/expander.c
+++ src/expander/expander.c
@@ -3,14 +3,43 @@
 /*                                                        :::      ::::::::   */
 /*   expander.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: maugusto <maugusto@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: gude-jes <gude-jes@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/13 12:05:58 by maugusto          #+#    #+#             */
-/*   Updated: 2024/08/13 12:06:00 by maugusto         ###   ########.fr       */
+/*   Updated: 2024/08/20 17:00:15 by gude-jes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void	detect_expansion(t_token *token)
+{
+	int		i;
+	bool	contains_sign;
+	bool	sq;
+	bool	dq;
+
+	i = 0;
+	contains_sign = false;
+	sq = false;
+	dq = false;
+	while(token->text[i])
+	{
+		if(token->text[i] == '\'' && !sq)
+			sq = !sq;
+		else if (token->text[i] == '"' && !dq)
+			dq = !dq;
+		else if (token->text[i] == '$' || token->text[i] == '\''
+			|| token->text[i] =='\"' )
+			contains_sign = true;
+		i++;
+	}
+	if (sq || dq)
+		return(0); //TODO: REPORTAR ERRO DE SINTAXE - SUBJECT
+	if (contains_sign)
+		return (1); // Tem expander
+	return(0); //Nao tem expander
+}
 
 char *expander(char *input)
 {
